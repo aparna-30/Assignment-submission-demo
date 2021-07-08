ee54e7eb3aa9c88e122bde69d2ee3a73e7afbf35
aparna-30 <87087261+aparna-30@users.noreply.github.com>
2021-07-08 21:58:25 +0530

    Add files via upload
    Day 3 Assignment 2-Palindrome

---
+++ palindrome.js
@@ -0,0 +1,12 @@
+//program to write whether the string is palindrone or not
+s="madam" 
+s1= ""
+ for(t of s)
+ { s1 = t+s1 } 
+ if( s == s1)
+ {
+     console.log("it is palindrone")
+ }
+ else
+ console.log("not palindrone")
+ 
