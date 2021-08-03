d3d1cc28dcfd81542bcdc4533b2866068ea0633f
aparna-30 <87087261+aparna-30@users.noreply.github.com>
2021-08-03 22:53:43 +0530

    Add files via upload
    Javascript essentials Assignment 1
    Take 2 variables and show all arthimetic operations

---
+++ assi_1.js
@@ -0,0 +1,18 @@
+let a = 10;
+let b = 20;
+let c = a + b;
+console.log("Addition of a and b is = " +c);
+
+let d = b - a;
+console.log("Subtraction of a and b is = "+d);
+
+let e = a * b;
+console.log("Multiplication of a and b is = "+e);
+
+let f = a / b;
+console.log("Division of a and b is = " +f);
+
+let g = b % a;
+console.log("Modolus of a and b is = " +g);
+
+
