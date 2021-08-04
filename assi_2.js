e06eff9ad8fcc401b74eba04d20e5e715470fb00
aparna-30 <87087261+aparna-30@users.noreply.github.com>
2021-08-04 17:41:09 +0530

    Add files via upload
    Assignment 2 of Javascript essentials
    1. create an array of 5 names and print in reverse
    2. print elements which is divisible by 5 from 1 to 50

---
+++ assi_2.js
@@ -0,0 +1,15 @@
+//1
+let arr = ['apple', 'banana' , 'watermelon' , 'kiwi' , 'papaya'];
+let i;
+for(i = arr.length-1 ; i >= 0 ; i--)
+{
+    console.log(arr[i]); 
+}
+
+//2
+let j;
+for(j = 1 ; j <= 50 ; j++)
+{
+    if(j % 5 == 0)
+    console.log(j);
+}
